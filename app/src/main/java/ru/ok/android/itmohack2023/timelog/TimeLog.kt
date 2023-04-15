947ddc9533ebea7eff5c0dbc01245ff536a3ba32
Kostya Razboynik <kkovalev2003@gmail.com>
2023-04-15 17:10:47 +0300

    change mutableList to arrayList

--- app/src/main/java/ru/ok/android/itmohack2023/timelog/TimeLog.kt
+++ app/src/main/java/ru/ok/android/itmohack2023/timelog/TimeLog.kt
@@ -4,17 +4,16 @@ import java.util.Calendar
 import kotlin.system.measureTimeMillis
 
 object TimeLog {
-    private val times: MutableList<Time> = ArrayList()
+    private val times = ArrayList<Time>()
 
     fun <T> measure(code: () -> T): T {
         val calendar = Calendar.getInstance()
         val start = calendar.timeInMillis
         var result: T
-        val time = measureTimeMillis {result = code()}
+        val time = measureTimeMillis { result = code() }
         times.add(Time(start, time))
         return result
     }
-
 
 
     data class Time(val callTime: Long, val timeInMillis: Long)
