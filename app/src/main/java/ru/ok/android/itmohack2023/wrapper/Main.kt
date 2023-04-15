5c5d009fcf691a8196ddfdeb7e1aa233fa522074
Владимир Назаров <vova24848@gmail.com>
2023-04-15 18:00:26 +0300

    Add Javassist main

---
+++ app/src/main/java/ru/ok/android/itmohack2023/wrapper/Main.kt
@@ -0,0 +1,20 @@
+package ru.ok.android.itmohack2023.wrapper
+
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import ru.ok.android.itmohack2023.timelog.TimeLog
+
+// use --add-opens java.base/java.lang=ALL-UNNAMED
+fun main() {
+    OkHttpWrapper().add()
+
+    val request: Request = Request.Builder()
+        .url("https://cat-fact.herokuapp.com/facts")
+        .build()
+
+
+    TimeLog.measure {
+        OkHttpClient().newCall(request).execute()
+            .use { response -> println(response.body?.string()) }
+    }
+}
