ce627114cdfcdffa689e63bbb16096343854fab0
Ildar Gainanov <ildargainanov@ya.ru>
2023-04-15 18:54:05 +0300

    Add Measure annotation

--- app/src/main/java/ru/ok/android/itmohack2023/Main.kt
+++ app/src/main/java/ru/ok/android/itmohack2023/Main.kt
@@ -1,10 +1,5 @@
 package ru.ok.android.itmohack2023
 
-import kotlin.reflect.KMutableProperty
-import kotlin.reflect.full.declaredMemberProperties
-import kotlin.reflect.full.memberFunctions
-import kotlin.reflect.full.memberProperties
-import kotlin.reflect.jvm.isAccessible
 
 class Bebra {
     fun a() : String {
@@ -21,13 +16,5 @@ fun main() {
     val bebra = Bebra()
     println(bebra.a())
 
-    val properties = Bebra::class.memberFunctions
 
-    val uuuuu  = properties.find {it.name == "a"} as KMutableProperty<*>?
-
-    uuuuu?.isAccessible = true;
-
-    uuuuu?.setter?.call(bebra, { -> "pepe" })
-
-    println(bebra.a())
 }
