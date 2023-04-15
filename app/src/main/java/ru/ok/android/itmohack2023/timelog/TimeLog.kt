5f41d14d9fbdf453d4e9fbbc2b40e256fc774f6e
mgvts <89970331+mgvts@users.noreply.github.com>
2023-04-15 19:04:20 +0300

    add refactor

--- app/src/main/java/ru/ok/android/itmohack2023/timelog/TimeLog.kt
+++ app/src/main/java/ru/ok/android/itmohack2023/timelog/TimeLog.kt
@@ -1,5 +1,7 @@
 package ru.ok.android.itmohack2023.timelog
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import java.util.Calendar
 import kotlin.random.Random
 import kotlin.system.measureTimeMillis
@@ -9,6 +11,8 @@ data class DTO(val nameFunction: String, val timeStart: Long, var timeDuration:L
 object TimeLog {
     private val times = ArrayList<Time>()
     private val pending = HashMap<Int, Long>()
+    @RequiresApi(Build.VERSION_CODES.O)
+    private var writer = Writer();
 
     fun start(): Int {
         var id: Int;
@@ -31,10 +35,10 @@ object TimeLog {
 
     fun <T> measure(code: () -> T): T {
         val calendar = Calendar.getInstance()
-        val start = calendar.timeInMillis
-        var result: T
-        val time = measureTimeMillis { result = code() }
-        times.add(Time(start, time))
+        val id = start()
+        var result = code()
+        end(id)
+
         return result
     }
 
