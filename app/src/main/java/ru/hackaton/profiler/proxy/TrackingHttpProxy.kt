0a19b07fa2cf799d19598e52699a0888b244c2f6
Ildar Gainanov <ildargainanov@ya.ru>
2023-04-16 10:57:27 +0300

    Various changes

---
+++ app/src/main/java/ru/hackaton/profiler/proxy/TrackingHttpProxy.kt
@@ -0,0 +1,4 @@
+package ru.hackaton.profiler.proxy
+
+class TrackingHttpProxy {
+}
--- app/src/main/java/ru/hackaton/profiler/timelog/TimeLog.kt
+++ app/src/main/java/ru/hackaton/profiler/timelog/TimeLog.kt
@@ -15,7 +15,8 @@ data class DTO(
     var duration: Long?,
     val name: String?,
     val url: String?,
-    var data_size: Long?
+    var data_size: Long?,
+    var error: Boolean = true
 )
 
 object TimeLog {
@@ -47,12 +48,13 @@ object TimeLog {
     }
 
 
-    fun end(id: Long) {
+    fun end(id: Long, error: Boolean = false) {
         val end = Calendar.getInstance().timeInMillis
         val start: Long = pending[id]!!
         pending.remove(id);
 
         values[id]!!.duration = end - start
+        values[id]!!.error = error
         var writer = Writer()
         values[id]?.let { writer.writeMess(it) }
         println("OK" + values[id]!!.url + " " + values[id]!!.name)
